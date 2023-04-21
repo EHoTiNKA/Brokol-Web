3005a1fafb4dc25f4ec45c3255364b27bc460c75
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-04-21 22:43:58 +0300

    footer_fix

---
+++ src/widgets/FooterBlock1.jsx
@@ -0,0 +1,11 @@
+import './styles/FooterBlock1.css'
+
+
+const FooterBlock1 = () => {
+    return(
+        <div className='firstBlock'></div>
+    );
+};
+
+
+export default FooterBlock1;
--- src/widgets/MainFooter.jsx
+++ src/widgets/MainFooter.jsx
@@ -1,19 +1,20 @@
-import './styles/MainFooter.css'
+import "./styles/MainFooter.css";
 import ContentColumn from "../components/ContentColumn";
-import BrandIconWhite from '../components/BrandIconWhite';
-
+import BrandIconWhite from "../components/BrandIconWhite";
+import FooterBlock1 from "./FooterBlock1";
 
 const MainFooter = () => {
-    return(
-        <div className='footer'>
-            <ContentColumn>
-                <div>
-                    <BrandIconWhite />
-                </div>
-            </ContentColumn>
+  return (
+    <div className="footer">
+      <ContentColumn>
+        <div className="footerContent">
+            <FooterBlock1>
+                <div className="block1Content">fdsfsf</div>
+            </FooterBlock1>
         </div>
-    );
+      </ContentColumn>
+    </div>
+  );
 };
 
-
-export default MainFooter
+export default MainFooter;
