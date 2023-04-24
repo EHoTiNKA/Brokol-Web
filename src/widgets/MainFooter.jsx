f465621eeb6d49cc68cc9b06f4ffaaa23b542c1d
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-04-24 12:05:35 +0300

    ready_footer

--- src/widgets/MainFooter.jsx
+++ src/widgets/MainFooter.jsx
@@ -4,24 +4,42 @@ import BrandIconWhite from "../components/BrandIconWhite";
 import FooterBlock from "./FooterBlock";
 import LanguageDropDown from "../components/LanguageDropDown";
 import BrocolHechTeg from "../components/BrocolHechTeg";
-import FooterSocialIcon from "../components/FooterSocialIcon";
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faInstagram, faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 
 const MainFooter = () => {
+  const icons = [faTwitter, faInstagram, faTelegram];
+
+  const [hovered, setHovered] = useState(new Array(icons.length).fill(false));
+  const toggleHover = (i) => {
+    const newHovered = [...hovered];
+    newHovered[i] = !newHovered[i];
+    setHovered(newHovered);
+  };
+
   return (
     <div className="footer">
       <ContentColumn>
         <div className="footerContent">
-            <FooterBlock>
-                <BrandIconWhite />
-            </FooterBlock>
-            <FooterBlock>
-              <div className="footerGrid2">
+          <FooterBlock>
+            <BrandIconWhite />
+          </FooterBlock>
+          <FooterBlock>
+            <div className="footerGrid2">
               <div className="firstGridCollumn">
-              <LanguageDropDown />
-              <BrocolHechTeg />
-              <FooterSocialIcon />
+                <LanguageDropDown />
+                <BrocolHechTeg />
+                <div className="socialIcons">
+                  {icons.map((icon, index) => (
+                    <FontAwesomeIcon
+                      icon={icon}
+                      className={hovered[index] ? "fa-shake fa-3x " : "fa-3x"}
+                      onMouseEnter={() => toggleHover(index)}
+                      onMouseLeave={() => toggleHover(index)}
+                    />
+                  ))}
+                </div>
               </div>
               <div className="secondGridCollumn">
                 <p className="gridSecondp">Об MegaBrocols</p>
@@ -34,15 +52,15 @@ const MainFooter = () => {
                 <p className="gridThirdp">Помощь</p>
                 <p className="gridThirdp">FAQs</p>
               </div>
-              </div>
-            </FooterBlock>
-            <FooterBlock>
-              <div className="footerGrid3">
+            </div>
+          </FooterBlock>
+          <FooterBlock>
+            <div className="footerGrid3">
               <p>© 2017 Uber Technologies Inc.</p>
               <p>Обработка персональных данных</p>
               <p>Пользовательское соглашение</p>
-              </div>
-            </FooterBlock>
+            </div>
+          </FooterBlock>
         </div>
       </ContentColumn>
     </div>
