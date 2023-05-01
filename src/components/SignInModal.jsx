8458edb3eaae253666a0121b4c1c6a751caf3db4
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-05-01 14:11:35 +0300

    modalEdit

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./styles/SignInModal.scss";
+import SignInIcon from "../widgets/SignInIcon";
 
 const SignInModal = () => {
-  
   return (
-    <div className="modalBackground">
-      <div className="signUpModal">
-        <div className="signUpForm">
-          <h1>SIGN IN</h1>
-          <div className="logInInvitation">
-            <h2>Welcome Back</h2>
-          </div>
+    <div className="signUpModal">
+      <div className="signUpForm">
+        <h1>SIGN IN</h1>
+      </div>
+      <SignInIcon />
+      <div className="secondGridModal">
+        <div className="logInInvitation">
+          <h2>Welcome Back</h2>
         </div>
       </div>
     </div>
-    
   );
 };
 
