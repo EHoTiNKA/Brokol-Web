146b0514891ddf99dad682dba742c2d98709cea2
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-05-01 16:36:51 +0300

    modal_fix

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -6,12 +6,31 @@ const SignInModal = () => {
   return (
     <div className="signUpModal">
       <div className="signUpForm">
-        <h1>SIGN IN</h1>
+        <h1>Регистрация</h1>
+        <p>Нет аккаунта, так давайте заведём!</p>
+        <div className="formGroup1">
+          <label className="labelName"> Имя пользователя</label>
+          <input className="inputName" placeholder="Ваше имя"></input>
+        </div>
+        <div className="formGroup2">
+          <label className="labelPass"> Пароль</label>
+          <input className="inputPass" placeholder="Ваш пароль"></input>
+        </div>
+        <div className="formGroup3">
+          <label className="labelMail"> E-mail</label>
+          <input className="inputMail" placeholder="Ваша почта"></input>
+        </div>
+
+        <button className="regButton">Регистрация</button>
       </div>
       <SignInIcon />
       <div className="secondGridModal">
         <div className="logInInvitation">
-          <h2>Welcome Back</h2>
+          <div className="secondModalContent">
+            <h2>Добро пожаловать обратно</h2>
+            <p>Готовы вернуться обратно к заказам?</p>
+            <button>Войти</button>
+          </div>
         </div>
       </div>
     </div>
