f2ea2fbd88266dd986205b8a29418b983f33f7c3
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-05-15 13:09:45 +0300

    dropDownRevork

--- src/components/BaseDropDown.jsx
+++ src/components/BaseDropDown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useOutsideClick from "../hooks/useOutsideClick";
 import "./styles/BaseDropDown.scss";
 
 const HIDDEN_STATE = { display: "none" };
@@ -6,20 +7,24 @@ const VISIBLE_STATE = { display: "block" };
 
 const BaseDropDown = ({ button, menu }) => {
   const [menuStyle, setMenuStyle] = useState(HIDDEN_STATE);
-  const changeDropDownState = () => {
-    if (menuStyle === HIDDEN_STATE) {
-      setMenuStyle(VISIBLE_STATE);
-    } else {
-      setMenuStyle(HIDDEN_STATE);
-    }
-  };
+
+
+  const openDropDown = () => {
+    setMenuStyle(VISIBLE_STATE);
+    console.log("openDropDown")
+  }
+
+  const closeDropDown = () => {
+    setMenuStyle(HIDDEN_STATE);
+  }
+
+  const ref = useOutsideClick(closeDropDown);
   return (
     <div className="ddBase">
-      <div onClick={changeDropDownState} className="ddBaseButton">
+      <div onClick={openDropDown} className="ddBaseButton">
         {button}
       </div>
-      <div style={menuStyle} className="ddBaseMenu">
-        <div className="ddBackGround" onClick={changeDropDownState} ></div>
+      <div ref={ref} style={menuStyle} className="ddBaseMenu">
         {menu}
       </div>
     </div>
